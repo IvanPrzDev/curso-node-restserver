6ef4abad1a759ee849fe19f7dd3e3e357068e3cf
Iván <ivanperezrebollo@hotmail.com>
2025-08-27 18:49:12 +0200

    Configure server to listen on environment port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const Server = require('./models/server');
+const port = process.env.PORT;
 
 const server = new Server();
 
-server.listen();
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on port ${port}`)
+});
