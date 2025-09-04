8fe6b80a0619d9717342b7dbf61b31d2d04f24a7
IvanDev <ivanperezrebollo@hotmail.com>
2025-09-04 13:22:59 +0200

    mongodb_cnn

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_ATLAS);
+        await mongoose.connect(process.env.MONGODB_CNN);
         console.log('Database connected');
     } catch (error) {
         console.error('Database connection error:', error);
